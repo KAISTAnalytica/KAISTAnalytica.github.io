49be6f88cb0e4b1d47600f4bdd3995a1e028901a
Luc Michels <3gmichels@gmail.com>
2018-10-31 14:02:19 +0900

    hello

---
+++ state.js
@@ -0,0 +1,20 @@
+
+
+function State(info, result=null) {
+    this.info = info
+}
+State.prototype.color = function() {
+	if(this.result == 0){
+    	return 'blue'
+	} else if (this.result == 1){
+		return 'red'
+    } else {
+    	return 'purple'
+    }
+
+}
+State.prototype.id = info.data().id
+State.prototype.name = info.attr(title)
+
+State.prototype.predictionRequest = function() {}
+
